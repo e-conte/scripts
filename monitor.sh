c61e164ffe58ad59dcd0aa543b1f38f4338f49ae
e-conte <160084751+e-conte@users.noreply.github.com>
2025-04-05 17:23:24 -0300

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+#  Custom Monitor Config
+#  xorg-xrandr required
 xrandr --newmode "2560x1080_68.00"  264.25  2560 2736 3008 3456  1080 1083 1093 1126 -hsync +vsync
 xrandr --addmode DisplayPort-0 2560x1080_68.00
 xrandr --output HDMI-A-0 --mode 1920x1080 --pos 292x0 --rotate normal --output DisplayPort-0 --primary --mode 2560x1080_68.00 --pos 0x1080 --rotate normal
+
